3454793962673f1e53de5835c593525d8880ca9b
Iver Marl Dumalag <138662321+IverDumalag@users.noreply.github.com>
2025-01-06 13:09:03 +0800

    admin-routing

---
+++ Client/hanapin_frontend/src/Admin/AdminPostManagement.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AdminSideBar from '../components/AdminSideBar';
+import AdminToolBar from '../components/AdminToolBar';
+
+const AdminPostManagement = () => {
+   return (
+      <div>
+         <AdminSideBar>
+            <AdminToolBar>
+            <h1>Admin Post Management</h1>
+            </AdminToolBar>
+         </AdminSideBar>
+      </div>
+   );
+};
+
+export default AdminPostManagement;
